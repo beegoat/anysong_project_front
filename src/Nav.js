0fecb8d32663625a9ff682e7b915e2c342ec2e9c
Sandy Lee <goatjswn@naver.com>
2022-11-26 19:23:34 +0900

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,53 @@
+function Nav(){
+    return(
+        <div className="navbar bg-base-100">
+            <div className="drawer">
+  <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+  <div className="drawer-content">
+
+    <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open drawer</label>
+  </div> 
+  <div className="drawer-side">
+    <label htmlFor="my-drawer" className="drawer-overlay"></label>
+    <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+
+      <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
+      
+    </ul>
+  </div>
+</div>
+            
+            <div className="flex-none">
+                
+{/* The button to open modal */}
+<label htmlFor="my-modal-3" className="btn">로그인</label>
+
+{/* Put this part before </body> tag */}
+<input type="checkbox" id="my-modal-3" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box relative">
+    <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+    <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
+    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+  </div>
+</div>
+{/* The button to open modal */}
+<label htmlFor="my-modal-3" className="btn">회원가입</label>
+
+{/* Put this part before </body> tag */}
+<input type="checkbox" id="my-modal-3" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box relative">
+    <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+    <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
+    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+  </div>
+</div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Nav;
---
+++ src/routes/Home.js
@@ -0,0 +1,9 @@
+
+
+function Home(){
+    return(
+    <strong>hi</strong>
+    )
+}
+
+export default Home;
