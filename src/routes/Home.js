020631e903b4549b66de352ee0e8400dea1b1cc1
Sandy Lee <goatjswn@naver.com>
2023-01-27 06:02:10 +0900

    ...

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -9,12 +9,12 @@ function Home(){
     const [mainNotice, setMainNotice] = useState([])
 
     const getTopRate = async() => {
-        const response = await axios.get('http://43.201.140.172/getTopRate')
+        const response = await axios.get('http://43.201.140.172:3001/getTopRate')
         setTopRating(response.data)
     }
 
     const getNotice = async() => {
-        const response = await axios.get('43.201.140.172/getnotice')
+        const response = await axios.get('43.201.140.172:3001/getnotice')
         setMainNotice(response.data[0])
     }
 
